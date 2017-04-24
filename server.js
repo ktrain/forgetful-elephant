6528c28d7b080de5614ca90fb68d97196142cad4
Kellen Steffen <kellen.steffen@thalmic.com>
2017-04-24 11:15:50 -0400

    Add CORS headers.

--- server.js
+++ server.js
@@ -5,6 +5,19 @@ const express = require('express');
 const app = express();
 const router = require('./router.js');
 
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin',   '*');
+	res.header('Access-Control-Allow-Headers',  'Cache-Control, Pragma, Origin, Content-Type');
+	res.header('Access-Control-Allow-Methods',  'POST, GET, PUT, DELETE, OPTIONS');
+	res.header('Access-Control-Expose-Headers', 'Content-Type');
+
+	if (req.type === 'OPTIONS') {
+		return res.status(200).send({});
+	}
+
+	next();
+});
+
 app.use(router);
 
 const port = process.env.PORT || 8000;
